0ce815f2338ff74d8ba633bf90d0e6195b7ce278
marips <99839074+marianafeitosa@users.noreply.github.com>
2023-02-13 16:18:12 -0300

    Add files via upload

---
+++ Projeto-2.php
@@ -0,0 +1,56 @@
+<html>
+ <head>
+
+ <title> Aula Exemplo do Uso de Variáveis </title>
+ </head>
+  
+ <body>
+
+ <?php 
+
+ // Declarando variáveis
+
+ $var1 = 1;
+ $varX = 9.50;
+ $varY = 9.50;
+ $var2 = $varX + $varY ;
+ $varSTR = "Atribui String";
+ $varletra = "a";
+ $varbool = true ;
+
+
+   echo "Exemplo de uma aplicação em PHP";
+   print "<P>";
+
+   echo "Conteúdo var1 :";
+   echo $var1;
+   print "<P>";
+
+   echo "Conteúdo varX :";
+   echo $varX;
+   print "<P>";
+
+   echo "Conteúdo varY :";
+   echo $varY;
+   print "<P>";
+
+   echo "Conteúdo var2 :";
+   echo $var2;
+   print "<P>";
+   
+   echo "Conteúdo varSTR :";
+   echo $varSTR;
+   print "<P>";
+
+   echo "Conteúdo varletra :";
+   echo $varletra;
+   print "<P>";
+
+   echo "Conteúdo varbool :";
+   echo $varbool;
+   print "<P>";
+
+ ?>
+ </body>
+
+</html>
